cffd554ad075f7c106a18c7170d4b6e48be217f6
Alex Sheldon <alexsheldon22@gmail.com>
2026-01-23 20:22:37 +0000

    adding Joins.py

---
+++ Joins.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+
+CPI_df = pd.read_csv("Datasets/CPIAUCSL.csv")
+ebp_df = pd.read_csv("Datasets/ebp_csv.csv")
+yield_df = pd.read_csv("Datasets/par-yield-curve-rates-1990-2023.csv")
+
+print(CPI_df.head())
+print(ebp_df.head())
+print(yield_df.head())
+
+"""------------------------------------------------------------------------
+Create Connects
+------------------------------------------------------------------------"""
+
+CPI_df['date'] = pd.to_datetime(CPI_df["observation_date"])
+CPI_df.head() 
+
+yield_df['date'] = pd.to_datetime['date']
+print(yield_df.head())
