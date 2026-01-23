6524307cb5b6aaea19c0cb01bc546e7421d56e8a
Alex Sheldon <alexsheldon22@gmail.com>
2026-01-23 21:21:28 +0000

    housing

--- Joins.py
+++ Joins.py
@@ -20,9 +20,30 @@ yield_df['date'] = pd.to_datetime(yield_df['date'])
 yield_df = yield_df[yield_df['date'].dt.day == 1]
 yield_df['date'] = yield_df["date"].dt.strftime("%Y-%m")
 
-big_df = pd.merge(CPI_df, yield_df, on='date', how='inner')
+# Housing combos
+h1 = pd.read_csv("HousingDev/COMPU1UNSA.csv")
+h2 = pd.read_csv("HousingDev/COMPU5MUNSA.csv")
+h3 = pd.read_csv("HousingDev/COMPU24UNSA.csv")
+h4 = pd.read_csv("HousingDev/COMPUTNSA.csv")
+
+hs = [h1,h2,h3,h4]
+for h in hs:
+    h['date'] = pd.to_datetime(h["observation_date"]).dt.strftime("%Y-%m")
+    h.drop('observation_date', axis=1, inplace=True)
+
+
+"""------------------------------------------------------------------------
+Merge Data
+------------------------------------------------------------------------"""
+
+
+big_df = pd.merge(CPI_df, yield_df, on='date', how='right', suffixes=('_cpi', '_yield'))
+
+for h in hs:
+    big_df = pd.merge(big_df, h, on='date', how='right', suffixes=('', '_housing'))
+
+big_df = big_df.sort_values('date', ascending=True)
+
 print(big_df.head())
-
-
 
 big_df.to_csv('dataocean.csv')
