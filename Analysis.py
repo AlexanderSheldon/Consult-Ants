d3f636e2ab4c530eab39fa019df0768ec925c385
Eric Pesci - Utah State University <ericpesci04@gmail.com>
2026-02-12 22:15:50 +0000

    Final VAR Changes

--- Analysis.py
+++ Analysis.py
@@ -1,12 +1,3 @@
 import pandas as pd
 import numpy as np
 
-CPI_df = pd.read_csv("Datasets/CPIAUCSL.csv")
-ebp_df = pd.read_csv("Datasets/ebp_csv.csv")
-yield_df = pd.read_csv("Datasets/par-yield-curve-rates-1990-2023.csv")
-
-print(CPI_df.head())
-print(ebp_df.head())
-print(yield_df.head())
-
-np.corrcoef(CPI_df['CPIAUCSL'], )
---
+++ VAR/PresentationSimulation.py
@@ -0,0 +1,10 @@
+import sys
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+# Import our custom modules
+from data_preparation import prepare_var_data
+from var_model import VARModelBuilder
+
+
