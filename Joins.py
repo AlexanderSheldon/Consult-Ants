c2caf28551fb0e3c41465f10d9f8d118aa64b3d5
Alex Sheldon <alexsheldon22@gmail.com>
2026-02-12 22:56:20 +0000

    cleaning the data ocean, go green!

--- Joins.py
+++ Joins.py
@@ -18,19 +18,13 @@ Create Month and year vars
 CPI_df['date'] = pd.to_datetime(CPI_df["observation_date"]).dt.strftime("%Y-%m")
 
 yield_df['date'] = pd.to_datetime(yield_df['date'])
-yield_df = yield_df[yield_df['date'].dt.day == 1]
-yield_df['date'] = yield_df["date"].dt.strftime("%Y-%m")
-
-# Housing combos
-h1 = pd.read_csv("HousingDev/COMPU1UNSA.csv")
-h2 = pd.read_csv("HousingDev/COMPU5MUNSA.csv")
-h3 = pd.read_csv("HousingDev/COMPU24UNSA.csv")
-h4 = pd.read_csv("HousingDev/COMPUTNSA.csv")
-
-hs = [h1,h2,h3,h4]
-for h in hs:
-    h['date'] = pd.to_datetime(h["observation_date"]).dt.strftime("%Y-%m")
-    h.drop('observation_date', axis=1, inplace=True)
+# Take the first observation available in each month
+yield_df['year_month'] = yield_df['date'].dt.to_period('M')
+yield_df = yield_df.sort_values('date').groupby('year_month', as_index=False).first()
+# Set date to first of month for consistency
+yield_df['date'] = yield_df['year_month'].dt.to_timestamp()
+yield_df = yield_df.drop('year_month', axis=1)
+yield_df['date'] = yield_df['date'].dt.strftime("%Y-%m")
 
 # GDP
 GDP_df = pd.read_excel("Datasets/US-Monthly-GDP-History-Data-Dec2025.xlsx", sheet_name='Data')
@@ -47,14 +41,15 @@ Merge Data
 
 big_df = pd.merge(CPI_df, yield_df, on='date', how='right', suffixes=('_cpi', '_yield'))
 
-for h in hs:
-    big_df = pd.merge(big_df, h, on='date', how='right', suffixes=('', '_housing'))
 
 big_df = big_df.sort_values('date', ascending=True)
 
 big_df = pd.merge(big_df, GDP_df, on='date', how='right')
 
 big_df = pd.merge(big_df, UN_df, on='date', how='right')
+
+# Filter data to 1992-01 through 2023-12
+big_df = big_df[(big_df['date'] >= '1992-01') & (big_df['date'] <= '2023-12')]
 
 print(big_df.head())
 
